77f893069c46bcdab87ad3d572a75e557b1bdb31
Bichi Kim <bichi@live.co.kr>
2018-06-18 11:05:35 +0900

    update

--- src/ApiServer.spec.js
+++ src/ApiServer.spec.js
@@ -1,8 +1,25 @@
 import ApiServer from './ApiServer'
 describe('APIServer', () => {
-  const server = new ApiServer({port: 3000, isLog: false})
   describe('start & stop', () => {
-    it('should start server', (done) => {
+    let server
+    it('should start server with configs in running constructor', (done) => {
+      server = new ApiServer({port: 3000, isLog: false})
+      expect(server.start).to.be.a('function')
+      server.start().then(({server: _server}) => {
+        expect(_server).to.be.an('object')
+        expect(_server.info.address).to.equal('127.0.0.1')
+        expect(_server.info.port).to.equal(3000)
+        server.stop().then(() => {
+          done()
+        }).catch((error) => {
+          done(error)
+        })
+      }).catch((error) => {
+        done(error)
+      })
+    })
+    it('should start server with configs in starting time', (done) => {
+      server = new ApiServer({port: 3000, isLog: false})
       expect(server.start).to.be.a('function')
       server.start().then(({server: _server}) => {
         expect(_server).to.be.an('object')
@@ -18,4 +35,5 @@ describe('APIServer', () => {
       })
     })
   })
+
 })
