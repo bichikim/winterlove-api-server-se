e3951e9497be4f6b619c14020dfbc1b6ac8465cb
Bichi Kim <bichi@live.co.kr>
2018-06-21 00:38:27 +0900

    update

--- src/ApiServer.ts
+++ src/ApiServer.ts
@@ -20,6 +20,9 @@ const DEFAULT_HOST = 'localhost'
 
 /**
  * ApiServer constructor & start Options
+ * Do not change update or change to add new Plugins
+ * only default plugins allow to add in this Api server
+ * otherwise use ApiServer.register(...)
  */
 export interface IServerOptions extends IArgvServerOptions {
   log?: ILogOptions
